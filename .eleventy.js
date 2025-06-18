36ee4b7430df4a7ee9c169245d2a3c3321554f3f
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2025-06-18 13:02:26 +0300

    feat: make the skeleton of the landing and organise the repo

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+import ejsPlugin from '@11ty/eleventy-plugin-ejs'
+
+
+export default function(config) {
+    config.addPlugin(ejsPlugin);
+
+    config.setEjsOptions({
+        _with:false,
+        localsName:'it'
+    })
+
+    config.addPassthroughCopy('src/assets')
+
+    return {
+        dir:{
+            input:"src",
+            output:"dist",
+            includes:"_includes",
+            layouts:"layouts",
+            data:"data"
+        }
+    }
+}
