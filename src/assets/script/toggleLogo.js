91c621a3e0ae6423c963639cec4b7a51cfb15c82
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2025-10-27 14:36:33 +0500

    feat: animate logo in the header

---
+++ src/assets/script/toggleLogo.js
@@ -0,0 +1,25 @@
+const hero = document.querySelector('#hero-page')
+const logo = document.querySelector('#header-logo')
+
+function handleIntersect(entries, observer){
+	entries.forEach(entry => {
+		if (!entry.isIntersecting){
+			logo.style.opacity = 1
+		} else {
+			logo.style.opacity = 0;
+		}
+	})
+}
+
+function createObserver(){
+	const options = {
+		root: null,
+		threshold: "0.15",
+		rootMargin: "0px",
+	}
+
+	const observer = new IntersectionObserver(handleIntersect, options);
+	observer.observe(hero);
+}
+
+createObserver()
