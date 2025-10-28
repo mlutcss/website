dcc68cf6d6d5e1c52af5f7fb32276bba932b741a
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2025-10-28 07:04:45 +0500

    fix: fix the behaviour of the logo in the header and refactor minor things

--- src/assets/script/toggleLogo.js
+++ src/assets/script/toggleLogo.js
@@ -1,25 +1,29 @@
-const hero = document.querySelector('#hero-page')
-const logo = document.querySelector('#header-logo')
+const hero = document.querySelector('#hero-page');
+const logo = document.querySelector('#header-logo');
 
 function handleIntersect(entries, observer){
 	entries.forEach(entry => {
 		if (!entry.isIntersecting){
-			logo.style.opacity = 1
+			logo.style.visibility = "visible";
+			logo.style.opacity = 1;
 		} else {
 			logo.style.opacity = 0;
-		}
-	})
-}
+			setTimeout(() => {
+				logo.style.visibility = "hidden";
+			}, 200);
+		};
+	});
+};
 
 function createObserver(){
 	const options = {
 		root: null,
 		threshold: "0.15",
 		rootMargin: "0px",
-	}
+	};
 
 	const observer = new IntersectionObserver(handleIntersect, options);
 	observer.observe(hero);
-}
+};
 
-createObserver()
+createObserver();
