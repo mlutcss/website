757e057e6a2353b94cae951155c42bc6866f68a1
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2026-02-01 16:01:07 +0500

    feat: add art interactivity with new custom element

--- src/assets/custom-elements/art-component.js
+++ src/assets/custom-elements/art-component.js
@@ -1,20 +1,52 @@
-const experimentClass = 'Ps-a T0 L0 M0 -Sz100p ^:h_Bgc#f00*0.5';
+const hightlightClasses = "Bd1;s;$brand Bxsd1;1;10;2;$brand Zi19"
 
 export class ArtComponent extends HTMLElement {
-	static observedAttributes = ['artId', 'art-background']
-
 	constructor() {
 		super();
 	}
 
 	connectedCallback() {
-		const p = document.createElement('p')
-		experimentClass.split(' ').forEach(c => {
-			p.classList.add(c)
-		})
-		this.appendChild(p)
-		this.style.position = "relative"
-		this.classList.add('-Ctx')
+		this.highlightTargetArt()
+		const artId = this.getAttribute('id');
+		console.log(artId);
+		const button = this.querySelector(`#${artId}-link-button`);
+		button.addEventListener('click', this.copyArtLink(artId))
+	}
+
+	copyArtLink = (artId) => {
+		return function() {
+			const span = this.querySelector('span')
+			let link = window.location.href
+			const idPosition = link.indexOf('#')
+			if (idPosition > 0) {
+				link.replace(
+					link.substring(idPosition + 1, link.length )	,
+					artId
+				)
+			} else {
+				link += `#${artId}`
+			}
+			navigator.clipboard.writeText(link)
+			span.innerText = 'Copied!'
+			setTimeout(() => {
+				span.innerText = 'Share';
+			}, 2000)
+		}
+	}
+
+	highlightTargetArt(){
+		const link = window.location.href
+		const idPosition = link.indexOf('#');
+		if (this.getAttribute('id') === link.substring(idPosition + 1, link.length )){
+			hightlightClasses.split(' ').forEach(c => {
+				this.classList.add(c)
+			})
+			setTimeout(() => {
+				hightlightClasses.split(' ').forEach(c => {
+					this.classList.remove(c)
+				})
+			},5000)
+		}
 	}
 }
 
