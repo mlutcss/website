505557586052855a4d31601a9cda1056aa1c49b6
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2026-02-03 16:36:18 +0500

    :fix: make some minor corrections and rewrite art-api logic

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
-import ejsPlugin from '@11ty/eleventy-plugin-ejs'
+import ejsPlugin from '@11ty/eleventy-plugin-ejs';
 
-export default function(config){
+export default async function(config){
   config.addPlugin(ejsPlugin,{
 		_with: false,
     localsName: 'it'
 	});
 
+	config.addCollection('art-api', (collectionApi) => {
+		const arts = collectionApi.getFilteredByGlob('src/arts/**/*.ejs')
+		arts.forEach(art => {
+			art.outputPath = `./dist/art-api/${art.fileSlug}/index.html`
+		})
+		return collectionApi.getFilteredByGlob('src/arts/**/*.ejs')
+	})
 
   config.addPassthroughCopy('src/assets')
 
--- src/assets/script/art-panel.js
+++ src/assets/script/art-panel.js
@@ -1,21 +1,18 @@
-const hightlightCss = "Bxsd1;1;10;2;$brand_t Bd1;s;$brand_t Zi19_t"
-
 export class ArtPanel extends HTMLElement {
 	constructor() {
 		super();
-		this.artId = this.getAttribute('id')
 		this.link = window.location.href
 	}
 
 	connectedCallback() {
 		this.highlightTargetArt();
-		const button = this.querySelector(`#${this.artId}-link-button`);
-		button.addEventListener('click', () => this.copyArtLink(button))
+		this.button = this.querySelector(`#${this.id}-link-button`);
+		this.button.addEventListener('click', () => this.copyArtLink())
 	}
 
-	copyArtLink = (button) => {
-		const span = button.querySelector('[data-name="button-text"]')
-		this.link = this.link.split('#')[0] + `#${this.artId}`
+	copyArtLink() {
+		const span = this.button.querySelector('.button-text')
+		this.link = this.link.split('#')[0] + `#${this.id}`
 		navigator.clipboard.writeText(this.link)
 		span.textContent = 'Copied!'
 		setTimeout(() => {
@@ -24,13 +21,9 @@ export class ArtPanel extends HTMLElement {
 	}
 
 	highlightTargetArt() {
-		hightlightCss.split(' ').forEach(c => {
-			this.classList.add(c)
-		})
+		this.classList.add('Bxsd1;1;10;2;$brand_t', 'Bd1;s;$brand_t', 'Zi19_t')
 		setTimeout(() => {
-			hightlightCss.split(' ').forEach(c => {
-				this.classList.remove(c)
-			})
+			this.classList.remove('Bxsd1;1;10;2;$brand_t', 'Bd1;s;$brand_t', 'Zi19_t')
 		},5000)
 	}
 }
